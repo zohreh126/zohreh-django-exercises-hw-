c8b49ad3773c4e5030b5f892843034d299c82c48
zohreh126 <zohrehpakseresht.3831@gmail.com>
2025-01-08 16:07:58 +0330

    admin part code

--- hw-24/product/admin.py
+++ hw-24/product/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
-
+from product.models import Category, Product
 # Register your models here.
+
+
+
+class AdminCategory(admin.ModelAdmin):
+    list_display=[
+        "cat_name"
+    ]
+
+class AdminProduct(admin.ModelAdmin):
+    list_display=[
+        "product_name","product_price", "Category_show"
+    ]
+
+
+    def Category_show (self, obj):
+        for i in obj.category.all():
+            return f"{i.cat_name}"
+
+
+admin.site.register(Product, AdminProduct)
+admin.site.register(Category, AdminCategory)
