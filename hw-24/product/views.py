0e02de1b24450f8b73601ae89aafc9860534a468
zohreh126 <zohrehpakseresht.3831@gmail.com>
2025-01-08 15:58:09 +0330

    views part code

--- hw-24/product/views.py
+++ hw-24/product/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Product, Category
     # Create your views here.
+    
+def product_show(request):
+    products=Product.objects.all()
+    return render(request,'' , {"products":products})
